13d76f0a192ecd7a60d812bc432ffb8f4a19cf46
jwkim <alsuehrckwl@naver.com>
2020-12-01 20:55:27 +0900

    about 작성중

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,28 +6,61 @@ import SEO from "../components/seo"
 
 import { Footer } from "../components/Footer/Footer"
 
-class About extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="All posts"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-
-        <h1>about me</h1>
-        <Footer />
-      </Layout>
-    )
-  }
+const About = (props) => {
+  const {data} = props;
+  const siteTitle = data.site.siteMetadata.title;
+  
+  return (
+          <Layout location={props.location} title={siteTitle}>
+            <SEO
+              title="About"
+              keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+            />
+    
+            
+            <div>
+              <h1>about me</h1>
+    
+              <p>Jeongwoo Kim. 김정우.</p>
+    
+              <h3>DEVELOPER / front-end</h3>
+    
+            </div>
+            <Footer />
+          </Layout>
+        )
 }
+
+// class About extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     const { data } = this.props
+//     const siteTitle = data.site.siteMetadata.title
+
+//     return (
+//       <Layout location={this.props.location} title={siteTitle}>
+//         <SEO
+//           title="About"
+//           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+//         />
+
+        
+//         <div>
+//           <h1>about me</h1>
+
+//           <p>Jeongwoo Kim. 김정우.</p>
+
+//           <h3>DEVELOPER / front-end</h3>
+
+//         </div>
+//         <Footer />
+//       </Layout>
+//     )
+//   }
+// }
 
 export default About
 
